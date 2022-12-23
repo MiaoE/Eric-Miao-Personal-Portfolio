6bd1581f100e5f9403435cd4ffa5c0a728478787
Eric Miao <ericxmiao@gmail.com>
2022-12-23 17:15:41 -0500

    main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
+      <About />
+      <Projects />
+      <Contact />
     </main>
     
   );
